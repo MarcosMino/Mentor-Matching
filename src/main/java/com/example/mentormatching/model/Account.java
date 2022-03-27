e7b05d430bd5d6c00d4094bfb7374fea3dff9d9a
Ash530888 <aishah.s.qureshi@gmail.com>
2022-03-27 20:04:04 +0100

    Changed package, Commenting Account.java

--- src/main/java/com/example/mentormatching/model/Account.java
+++ src/main/java/com/example/mentormatching/model/Account.java
@@ -1,16 +1,36 @@
-package com.example.mentormatching.model;
+/*
+ * @author Aishah Qureshi aishah.s.qureshi@gmail.com | 200100227
+ */
+
+package com.company.src.main.java.com.example.mentormatching.model;
 
 public class Account {
     private String email;
     private String password;
     private String pin;
 
+
+    /*
+     * Constructor
+     *
+     * @param  String  email
+     * @param  String  password
+     * @param  String  pin
+     * @return null
+     */
     public Account(String email, String password, String pin){
         this.email=email;
         this.password=password;
         this.pin=pin;
     }
 
+    /*
+     * Constructor
+     *
+     * @param  String  email
+     * @param  String  password
+     * @return null
+     */
     public Account(String email, String password){
         this.email=email;
         this.password=password;
