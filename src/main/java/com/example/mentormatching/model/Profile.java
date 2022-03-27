e7e3dafd367b79eb0a7c3abd2fc7994edae4f941
leena-n <77998902+leena-n@users.noreply.github.com>
2022-03-27 23:01:13 +0100

    Profile.java

---
+++ src/main/java/com/example/mentormatching/model/Profile.java
@@ -0,0 +1,60 @@
+/*
+ * @author Leena Neyazi ec20300@qmul.ac.uk | 200201188
+ */
+
+package com.company.src.main.java.com.example.mentormatching.model;
+
+public class Profile {
+    private User profileUser;
+    private Fields[] fields;
+
+     /*
+     * Constructor
+     *
+     * @param  User  profileUser
+     * @return null
+     */
+    public Profile(User profileUser){
+        this.profileUser = profileUser;
+    }
+
+    public User getProfileUser(){
+        return profileUser;
+    }
+
+    public Field getField(String fieldName){
+        int n = fields.length;
+        for (i = 0; i < n; i++) {
+            if ((fields[i].getName()).equals(fieldName)){
+                return fields[i];
+            }
+        }
+    }
+
+    public void editField(String fieldName, Field newField){
+    
+    }
+
+    public Field[] getFields(){
+        return fields;
+    }
+
+    public void addField(Field field){
+        int n = fields.length;
+        Field newArr[] = new Field[n + 1];
+        for (i = 0; i < n; i++) {
+            newArr[i] = fields[i];
+        }
+        newArr[n] = field;
+    }
+
+    public boolean hasField(String fieldName){
+        int n = fields.length;
+        for (i = 0; i < n; i++) {
+            if ((fields[i].getName()).equals(fieldName)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
