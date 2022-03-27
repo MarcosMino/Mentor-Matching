65c82784f2db688ceeabd789a4da0d7b2ba0bfbb
martaweronikamw <101061129+martaweronikamw@users.noreply.github.com>
2022-03-27 13:47:55 +0100

    Create AdministrativeUser.java

---
+++ AdministrativeUser.java
@@ -0,0 +1,22 @@
+package com.company;
+
+class AdministrativeUser {
+
+    public AdministrativeUser(){
+        AdministrativeUser adminUser = new AdministrativeUser();
+    }
+
+    public Account createAccount(){
+
+    }
+
+    public Request verifyMentorRoleRequest(){
+
+    }
+
+    public boolean removeProfile(profile Profile){
+        
+    }
+
+
+}
